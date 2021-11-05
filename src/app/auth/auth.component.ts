2852e64d181aa2620cf66c3a92df8fd93f774b93
Telmo Vieira <telmo_vieira2rhotmail.com>
2021-11-05 16:23:34 +0000

    Authentication and Route Protect part 1

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -22,6 +22,10 @@ export class AuthComponent {
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
+        this.sucessSign = "";
+        this.sucessLogin = "";
+        this.sucessSignBol = false;
+        this.sucessLoginBol = false;
     }
 
     onSubmit(form: NgForm) {
@@ -37,28 +41,25 @@ export class AuthComponent {
         this.error = '';
         if (this.isLoginMode) {
             authObs = this.authService.login(email, password);
+            this.sucessLoginBol = true;
+            this.sucessLogin = "Login sucess!!"
         } else {
             authObs = this.authService.signUp(email, password);
+            this.sucessSignBol = true;
+            this.sucessSign = "Sign up sucess!!"
         }
 
         authObs.subscribe(resData => {
             console.log(resData);
-            this.isLoading = false;
-            if(this.isLoginMode){
-               this.sucessLoginBol = true;
-                this.sucessLogin = "Login sucess!!"
-            } else {
-                this.sucessSignBol = true;
-                this.sucessSign = "Sign up sucess!!"
-            }
-           
+            this.isLoading = false;           
         },
             errorMessage => {
                 this.error = errorMessage;
                 this.isLoading = false;
+                this.sucessSignBol = false;
+                this.sucessLoginBol = false;
             }
         );
-
         form.reset();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
 
-import { HttpClient } from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+
+import { User } from "./user.model";
 
 export interface AuthResponseData {
     kind: string;
@@ -15,48 +17,55 @@ export interface AuthResponseData {
 }
 
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
-    constructor(private http: HttpClient) {}
-    
+
+    user = new Subject<User>();
+
+    constructor(private http: HttpClient) { }
+
     signUp(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCSi24nnR2cvRT-h-Y0KkZxZQzip08dJ-U', 
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        }
-      ).pipe(catchError(errorRes => {
-          let errorMessage = 'An unknown error ocurred!';
-          if(!errorRes.error || !errorRes.error.error) {
-              return throwError(errorMessage);
-          }
-          switch(errorRes.error.error.message) {
-              case 'EMAIL_EXISTS':
-                errorMessage = 'This email exist already';
-          }
-          return throwError(errorMessage);
-      }));
-        
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCSi24nnR2cvRT-h-Y0KkZxZQzip08dJ-U',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError), tap(resData => {
+            const expiratonDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
+            const user = new User(resData.email, resData.localId, resData.idToken, expiratonDate);
+            this.user.next(user);
+        }));
     }
 
     login(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCSi24nnR2cvRT-h-Y0KkZxZQzip08dJ-U', 
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCSi24nnR2cvRT-h-Y0KkZxZQzip08dJ-U',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error ocurred!';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
         }
-      ).pipe(catchError(errorRes => {
-          let errorMessage = 'An unknown error ocurred!';
-          if(!errorRes.error || !errorRes.error.error) {
-              return throwError(errorMessage);
-          }
-          switch(errorRes.error.error.message) {
-              case 'EMAIL_EXISTS':
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
                 errorMessage = 'This email exist already';
-          }
-          return throwError(errorMessage);
-      }));
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'Password invalid!';
+                break;
+
+
+        }
+        return throwError(errorMessage);
     }
 }
